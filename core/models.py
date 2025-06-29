f3e51711669fcfbecef9fa8830db8110d0a24519
TYPHYEL <typhanieyn@icloud.com>
2025-06-29 13:06:46 +0100

    Ajout du fichier .gitignore et mise à jour des modèles, sérialiseurs et vues

--- core/models.py
+++ core/models.py
@@ -30,6 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20, blank=True, null=True, verbose_name='Téléphone')
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='CLIENT')
     date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
