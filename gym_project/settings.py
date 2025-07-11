e22b251a91b6619276d2eec79c597f89f86688b0
TYPHYEL <typhanieyn@icloud.com>
2025-07-11 21:46:22 +0100

    Update settings.py

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -180,11 +180,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+
