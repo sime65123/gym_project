7ab7ae347e8ecd97eb6a444ea92bad5aa0187a2e
TYPHYEL <typhanieyn@icloud.com>
2025-07-11 22:11:14 +0100

    Update settings.py

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -177,6 +177,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
