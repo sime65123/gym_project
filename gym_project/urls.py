2c0b62989047463dcdd12defe9c4df74342dffb1
TYPHYEL <typhanieyn@icloud.com>
2025-07-13 23:35:51 +0100

    Update urls.py

--- gym_project/urls.py
+++ gym_project/urls.py
@@ -1,35 +1,17 @@
-"""
-URL configuration for gym_project project.
+# gym_project/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework import routers
-from core.views import AbonnementClientViewSet
-
-router = routers.DefaultRouter()
-router.register(r'abonnements-clients', AbonnementClientViewSet, basename='abonnementclient')
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    
+    # Toutes les routes de ton API (y compris JWT, ViewSets, etc.)
     path('api/', include('core.urls')),
 ]
 
+# Pour le développement : servir les fichiers media (images, etc.)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
